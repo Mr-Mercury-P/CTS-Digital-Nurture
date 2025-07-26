08ca981f19d90f0cac8a98b870007cfe0b059975
Puli Prabhas Reddy <141412556+Mr-Mercury-P@users.noreply.github.com>
2025-07-26 20:34:56 +0530

    added week 6 assignment 4

---
+++ Week6/blogapp/src/App.js
@@ -0,0 +1,12 @@
+import Posts from './Posts';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to BlogApp</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
